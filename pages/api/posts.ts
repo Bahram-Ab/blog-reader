516a44d8b32366b53095d8ef378b10b011b6266e
Bahram-Ab <bahram.abdorrahimi@gmail.com>
2023-02-25 21:43:52 +0330

    post api created, staticGeneration added

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,13 +1,28 @@
 import { NextApiHandler } from "next"
+import matter from "gray-matter"
+import fs from "fs"
+import path from "path"
 
 const handler: NextApiHandler = (req, res) => {
   const { method } = req
   switch (method) {
     case "GET":
-      return res.json({ ok: true })
+      const data = readPostsInfo()
+      return res.json({ postInfo: data })
     default:
       res.status(404).send("not found")
   }
+}
+
+const readPostsInfo = () => {
+  const dirPathToRead = path.join(process.cwd(), "posts")
+  const dirs = fs.readdirSync(dirPathToRead)
+  const data = dirs.map((fileName) => {
+    const filePathToRead = path.join(process.cwd(), "posts/" + fileName)
+    const fileContent = fs.readFileSync(filePathToRead, { encoding: "utf-8" })
+    return matter(fileContent).data
+  })
+  return data
 }
 
 export default handler
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,13 +1,30 @@
 import BlogCard from "@/components/BlogCard"
-import { NextPage } from "next"
+import { InferGetStaticPropsType, NextPage } from "next"
 
-const Blog: NextPage = () => {
+interface postApiResponse {
+  title: string
+  meta: string
+  slug: string
+}
+
+export const getStaticProps = async () => {
+  const data: postApiResponse[] = await fetch("http://localhost:3000/api/posts")
+    .then((res) => res.json())
+    .then((res) => res.postInfo)
+
+  return {
+    props: { posts: data }
+  }
+}
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>
+
+const Blog: NextPage<Props> = ({ posts }) => {
   return (
     <div className="mx-auto p-5 max-w-3xl space-y-5">
-      <BlogCard
-        title="this is my title"
-        desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum sapiente consequuntur laudantium amet veritatis! Quisquam esse eius numquam quod beatae!"
-      />
+      {posts.map(({ title, meta, slug }) => {
+        return <BlogCard key={slug} title={title} desc={meta} />
+      })}
     </div>
   )
 }
