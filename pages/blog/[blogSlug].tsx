ab2927c14112984cdb00be7909a269b4f2badbd0
Bahram-Ab <bahram.abdorrahimi@gmail.com>
2023-03-01 11:12:54 +0330

    make .md files readable

--- pages/blog/[blogSlug].tsx
+++ pages/blog/[blogSlug].tsx
@@ -7,18 +7,20 @@ import {
 import path from "path"
 import fs from "fs"
 import matter from "gray-matter"
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote"
+import { serialize } from "next-mdx-remote/serialize"
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>
 
-const singleBlogPage: NextPage<Props> = (props) => {
+const singleBlogPage: NextPage<Props> = ({ blog }) => {
+  const { content, title } = blog
   return (
-    <div>
-      <div>{props.blog.title}</div>
-      <div>{props.blog.content}</div>
+    <div className="prose lg:prose-xl mx-auto py-10">
+      <h1 className="text-2xl border-b-4 border-stone-500 pb-5">{title}</h1>
+      <MDXRemote {...content} />
     </div>
   )
 }
-
 export default singleBlogPage
 
 export const getStaticPaths: GetStaticPaths = () => {
@@ -37,23 +39,22 @@ export const getStaticPaths: GetStaticPaths = () => {
 
 type Blog = {
   blog: {
-    content: string
+    content: MDXRemoteSerializeResult
     title: string
   }
 }
 
-export const getStaticProps: GetStaticProps<Blog> = (context) => {
+export const getStaticProps: GetStaticProps<Blog> = async (context) => {
   const { params } = context
   const { blogSlug } = params!
   const filePathToRead = path.join(process.cwd(), "posts/" + blogSlug + ".md")
   const fileContent = fs.readFileSync(filePathToRead, { encoding: "utf-8" })
-  const { content, data } = matter(fileContent)
-
+  const source: any = await serialize(fileContent, { parseFrontmatter: true })
   return {
     props: {
       blog: {
-        content,
-        title: data.title
+        content: source,
+        title: source.frontmatter.title
       }
     }
   }
